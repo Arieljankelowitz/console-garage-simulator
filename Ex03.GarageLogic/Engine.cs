c0a7731b96c19604acf3d0f88382ccef10da751c
ariel <arieljankelowitz@gmail.com>
2024-06-10 16:27:15 +0300

    halt Engine

---
+++ Ex03.GarageLogic/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Engine
+    {
+        float m_RemainingBatteryLife;
+        float m_MazBatteryLife;
+        public Engine() { }
+        
+    }
+}
