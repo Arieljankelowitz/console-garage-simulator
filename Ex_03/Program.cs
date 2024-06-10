26c85f0dfc34618412c42df232f44d6deb62e0b5
ariel <arieljankelowitz@gmail.com>
2024-06-10 16:18:52 +0300

    done wheel

--- Ex_03/Program.cs
+++ Ex_03/Program.cs
@@ -1,12 +1,14 @@
 ﻿
 
+using Ex03.GarageLogic;
+
 namespace Ex_03
 {
     internal class Program
     {
         static void Main()
         {
-            System.Console.WriteLine("holle world");
+;
         }
     }
 }
